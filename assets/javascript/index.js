7ba052cb0b9753a0e03ebb8cc80fb66255329b9a
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-10-28 11:18:56 -0500

    Productos completo

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,15 @@
-import chalk from "chalk";
 const log = console.log;
 
-log(chalk.green('Estamo activo'));
+const openSearch = document.getElementById('navbar-search-open');
+const closeSearch = document.getElementById('search-form-close');
+const searchArea = document.querySelector('.navbar-search-form');
+const textSearchArea = document.getElementById('search-text');
+
+const searchActive =()=>{
+    searchArea.classList.toggle('search-form');
+    textSearchArea.focus();
+    textSearchArea.value = "";
+}
+
+openSearch.addEventListener('click',searchActive);
+closeSearch.addEventListener('click',searchActive);
