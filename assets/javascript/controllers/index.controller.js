00d3519e587995a3789d7096623376761d541991
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-10-29 15:15:33 -0500

    Carga de JSON completa

--- assets/javascript/controllers/index.controller.js
+++ assets/javascript/controllers/index.controller.js
@@ -1,7 +1,6 @@
 import { Product } from "../components/product.class.js";
 import { clientServices } from "../service/cliente-service.js";
 const log = console.log;
-const productList = [];
 
 const openSearch = document.getElementById('navbar-search-open');
 const closeSearch = document.getElementById('search-form-close');
@@ -17,9 +16,60 @@ const searchActive =()=>{
 openSearch.addEventListener('click',searchActive);
 closeSearch.addEventListener('click',searchActive);
 
+const createSection = (title)=>{
+    const id = title.replace(" ","-").toLowerCase();
+    const sectionInnerHTML = `
+    <section class="products" id="${id}">
+        <div class="products__title">
+            <h2 class="products__title-content"><a class="products__title-link" href="#">Artículos: ${title} <img class="products__title-link-img" src="./assets/images/icons/right.svg" alt="icono"></a></h2>
+        </div>
+        <div class="products-container products-scroller" data-products="${title}">
+        </div>
+    </section>
+    `;
+    return sectionInnerHTML;
+}
 
-clientServices.productList().then((data)=>{
+const createProduct = (product) =>{
+    let like = 'alike';
+    if(product.belike){ like = "like"}
+    const productInnerHTML = `
+    <div class="product card-view" data-product="${product.id}">
+        <img class="product__mark" src="./assets/images/icons/${like}.svg" alt="icono">
+        <img class="product__preview" src=${product.cover} alt="preview de producto">
+        <div class="product-content">
+            <span class="product-content__price-before">$${product.descount.priceBefore}</span>
+            <p class="product-content__price">$${product.price} <span class="product-content__descount">${product.descount.porcent} OFF</span></p>
+            <p class="product-content__details">${product.name}</p>
+        </div>
+    </div>
+    `;
+
+    return productInnerHTML;
+}
+
+const main = document.querySelector("main");
+const categories = new Set();
+const productList = []
+
+clientServices.productList("REMOTO")
+.then((data)=>{
     data.forEach(product => {
+        categories.add(product.category);
         productList.push(product);
     });
-});
+})
+.finally(()=>{
+    categories.forEach((category)=>{
+        main.innerHTML += createSection(category)
+        const productContainer = document.querySelector(`[data-products="${category}"]`);
+        productList.forEach((product)=>{
+            if(product.category === category){
+                productContainer.innerHTML += createProduct(product);
+            }
+        })
+    })
+
+})
+
+
--- assets/javascript/service/cliente-service.js
+++ assets/javascript/service/cliente-service.js
@@ -1,4 +1,17 @@
-const productList = () => fetch("http://localhost:3000/products").then((response)=>response.json());
+const URL = "http://localhost:3000/products";
+const localData = "../../../server/db.local.json";
+
+const productList = async(access = "LOCAL") => {
+    let data; 
+    try {
+        if(access==="REMOTO"){data = await fetch(URL)};
+        if(access==="LOCAL"){data = await fetch(localData)};
+    } catch (error) {
+        console.log("Sin conexión a las bases de datos");
+        data = await fetch(localData)
+    }
+    return await data.json();
+}
 
 export const clientServices = {
     productList,
