e991e18fae9afa396bf6a4a66cf1d5dfc20da627
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-10-30 08:17:44 -0500

    Estilos para la seccion: Consolas

--- assets/javascript/controllers/index.controller.js
+++ assets/javascript/controllers/index.controller.js
@@ -18,12 +18,14 @@ closeSearch.addEventListener('click',searchActive);
 
 const createSection = (title)=>{
     const id = title.replace(" ","-").toLowerCase();
+    let scrollerActive = "products-scroller"
+    if(title === "Consolas"){scrollerActive = ""}
     const sectionInnerHTML = `
     <section class="products" id="${id}">
         <div class="products__title">
             <h2 class="products__title-content"><a class="products__title-link" href="#">Artículos: ${title} <img class="products__title-link-img" src="./assets/images/icons/right.svg" alt="icono"></a></h2>
         </div>
-        <div class="products-container products-scroller" data-products="${title}">
+        <div class="products-container ${scrollerActive}" data-products="${title}">
         </div>
     </section>
     `;
@@ -31,15 +33,17 @@ const createSection = (title)=>{
 }
 
 const createProduct = (product) =>{
-    let like = 'alike';
+    let like = "alike";
+    let activeDescount = "";
     if(product.belike){ like = "like"}
+    if(!product.descount.active){ activeDescount = "not"}
     const productInnerHTML = `
     <div class="product card-view" data-product="${product.id}">
         <img class="product__mark" src="./assets/images/icons/${like}.svg" alt="icono">
         <img class="product__preview" src=${product.cover} alt="preview de producto">
         <div class="product-content">
-            <span class="product-content__price-before">$${product.descount.priceBefore}</span>
-            <p class="product-content__price">$${product.price} <span class="product-content__descount">${product.descount.porcent} OFF</span></p>
+            <span class="product-content__price-before ${activeDescount}">$${product.descount.priceBefore}</span>
+            <p class="product-content__price">$${product.price} <span class="product-content__descount ${activeDescount}">${product.descount.porcent} OFF</span></p>
             <p class="product-content__details">${product.name}</p>
         </div>
     </div>
