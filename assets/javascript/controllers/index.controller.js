d23aaf722ef4c7926618d88826ef8e2644103c24
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-11-01 16:16:16 -0500

    Interfaz de producto individual

--- assets/javascript/controllers/index.controller.js
+++ assets/javascript/controllers/index.controller.js
@@ -24,10 +24,10 @@ const createProduct = (product) =>{
     if(product.belike){ like = "product__mark--like"}
     if(!product.descount.active){ activeDescount = "not"}
     const productInnerHTML = `
-    <div class="product card-view" data-product="${product.id}">
+    <div class="product card-view" >
         <div class="product__mark ${like}" data-like></div>
         <img class="product__preview" src=${product.cover} alt="preview de producto">
-        <div class="product-content">
+        <div class="product-content" data-product="${product.id}">
             <span class="product-content__price-before ${activeDescount}">$${product.descount.priceBefore}</span>
             <p class="product-content__price">$${product.price} <span class="product-content__descount ${activeDescount}">${product.descount.porcent} OFF</span></p>
             <p class="product-content__details">${product.name}</p>
